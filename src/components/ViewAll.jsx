75f9b250ea517b669132025f5348925c83e192f3
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-07 13:49:01 +0530

    completed

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const ViewAll = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Published Year</th>
+      <th scope="col">price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+    <tr>
+      <th scope="row">xyz</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Rs 350</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default ViewAll
